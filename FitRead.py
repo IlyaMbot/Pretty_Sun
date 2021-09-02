3c2ece4bd813a36a1087635f1b379920dea73b7f
IlyaMbot <ilymotyk@gmail.com>
2021-09-02 15:54:34 +0800

    Initial commit

---
+++ FitRead.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import glob, os
+from scipy import ndimage
+from astropy.visualization import astropy_mpl_style
+from astropy.utils.data import get_pkg_data_filename
+from astropy.io import fits
+
+
+def plot_picture(image, inf = 0, sup = 1, step = 1, colours = 'hot' ):
+    ''' It plots figures '''
+    plt.style.use(astropy_mpl_style) #telling astropy about using plt
+    plt.figure()
+    plt.grid(b=None)
+    #plt.imshow(image,  extent=[-size, size, -size, size], cmap = colours)
+    plt.plot()
+    plt.show()
+
+
+filenames = glob.glob('/home/conpucter/Desktop/DataSRH/2016/18/*')
+filenames = sorted(filenames, key=os.path.basename)
+
+for filename in filenames[0:1]:
+	image_file = fits.open(filename)
+	print(image_file[0].header)
+	data = image_file[1].data
+	print(np.shape(data))
+	#plot_picture(data)
+
